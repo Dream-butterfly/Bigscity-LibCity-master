0c9a268deec14888d7d284f6fd9683491b8bccbe
Ziqi Lu <34276962+Dream-butterfly@users.noreply.github.com>
2026-03-26 16:24:07 +0800

    添加超参数调优兼容性模块，更新.gitignore以排除缓存文件

---
+++ libcity/core/__init__.py
@@ -0,0 +1,10 @@
+from libcity.core.factory import create_dataset, create_evaluator, create_executor, create_model
+from libcity.core.registry import Registry
+
+__all__ = [
+    "Registry",
+    "create_dataset",
+    "create_evaluator",
+    "create_executor",
+    "create_model",
+]
---
+++ libcity/core/factory.py
@@ -0,0 +1,26 @@
+def create_dataset(config):
+    from libcity.data.registry import get_dataset_class
+
+    dataset_class = get_dataset_class(config["dataset_class"])
+    return dataset_class(config)
+
+
+def create_model(config, data_feature):
+    from libcity.model.registry import get_model_class
+
+    model_class = get_model_class(config["task"], config["model"])
+    return model_class(config, data_feature)
+
+
+def create_executor(config, model, data_feature):
+    from libcity.executor.registry import get_executor_class
+
+    executor_class = get_executor_class(config["executor"])
+    return executor_class(config, model, data_feature)
+
+
+def create_evaluator(config):
+    from libcity.evaluator.registry import get_evaluator_class
+
+    evaluator_class = get_evaluator_class(config["evaluator"])
+    return evaluator_class(config)
